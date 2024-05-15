fd19dd8eb7550c0b1bed2d10a83bce40b2ed3f34
kevin.pruvost <pruvostkevin0@gmail.com>
2024-05-15 16:48:38 +0800

    main quick cleanup

--- VenomEngine/main.cpp
+++ VenomEngine/main.cpp
@@ -1,24 +1,6 @@
-#include <common/Engine/EngineLoader.h>
-#include <common/Logger.h>
-
-#include <stdexcept>
-#include <iostream>
-
-#include <common/Window.h>
-#include <common/Shader.h>
-#include <common/ShaderPipeline.h>
-#include <common/Model.h>
-#include <common/Memory.h>
-#include <common/Math_Base.h>
-#include <common/Camera.h>
-#include <common/GUI.h>
-#include <common/Texture.h>
-#include <common/Components/Transform.h>
-#include <common/Time.h>
-#include <common/Text2D.h>
-#include "Scene.h"
-
 #include <common/VenomEngine.h>
+
+#include "Scene.h"
 
 int main()
 {
